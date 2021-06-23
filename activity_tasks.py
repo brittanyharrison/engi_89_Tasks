624ef19741f1ef6aa08b4f9788c4788389bbc9df
Brittany Harrison <bkharrison94@gmail.com>
2021-06-23 13:32:14 +0100

    while statement changed

--- activity_tasks.py
+++ activity_tasks.py
@@ -4,23 +4,20 @@ The instructions for these activities acan be found on the read
 # define a variable `name`, and assign a string
 name = "name"
 # re assign the original variable with your name
-name = "Brittany"
+name = input("Whats your name?")
 
 # use concatenation to print a welcome message
 # use methods to format the name/input (remove white spaces)
-greetings = "Hi " + name + "! Welcome to Sparta Global."
-#greetings.strip()
+greetings = "Hi " + name.strip() + "! Welcome to Sparta Global."
 print(greetings.strip())
 
 # Version 2 - with interpolation
 
 # define another variable `full_name` to a random string
-full_name = "random string"
+full_name = "full name"
 
-while full_name != "quit":
+while True:
     # re assign the variable `full_name` to a name
     full_name = input("What is your full name?")
     # use interpolation to print the message
     print(f'Hello {full_name.title()}! This is python')
-
-
